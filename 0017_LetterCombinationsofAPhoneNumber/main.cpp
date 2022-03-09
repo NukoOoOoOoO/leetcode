26642aad984f2f3b3717fa9ab219f20cbc66f95d
󠀡󠀡 󠀡󠀡 <nukoooo@nukooo.cc>
2022-03-09 22:38:30 +0800

    a

---
+++ 0017_LetterCombinationsofAPhoneNumber/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+class Solution
+{
+    std::unordered_map<char, std::string> nums = {
+        { '1', "" }
+        { '2', "abc" },
+        { '3', "def" },
+        { '4', "ghi" },
+        { '5', "jkl" },
+        { '6', "mno" },
+        { '7', "pqrs"},
+        { '8', "tuv" },
+        { '9', "wxyz"},
+    };
+
+    std::vector<std::string> _results {};
+    std::string _tempString {};
+
+    void process(const std::string& str, int idx)
+    {
+        if (idx == str.size())
+        {
+            _results.push_back(_tempString);
+            return;
+        }
+
+        auto& letters = nums[str[idx]];
+
+        for (auto i = 0; i < letters.size(); i++)
+        {
+            _tempString.push_back(letters[i]);
+            process(str, idx + 1);
+            _tempString.pop_back();
+        }
+    }
+
+public:
+    std::vector<std::string> letterCombinations(std::string digits)
+    {
+        _tempString.clear();
+        _results.clear();
+        if (digits.empty())
+            return _results;
+        process(digits, 0);
+        return _results;
+    }
+};
+
+int main()
+{
+    Solution s;
+
+    auto res = s.letterCombinations("23");
+    for (auto i : res)
+        std::cout << i << " ";
+    std::cout << std::endl;
+
+    res = s.letterCombinations("8964");
+    for (auto i : res)
+        std::cout << i << " ";
+    std::cout << std::endl;
+
+    res = s.letterCombinations("1");
+    for (auto i : res)
+        std::cout << i << " ";
+    std::cout << std::endl;
+
+    std::cout << std::endl;
+    res = s.letterCombinations("12");
+    for (auto i : res)
+        std::cout << i << " ";
+    std::cout << std::endl;
+
+    res = s.letterCombinations("2");
+    for (auto i : res)
+        std::cout << i << " ";
+
+}
---
+++ 0058_LengthOfLastWord/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+class Solution
+{
+public:
+    int lengthOfLastWord(std::string str)
+    {
+        str.erase(std::find_if(str.rbegin(), str.rend(), [](unsigned char s) {
+            return !std::isspace(s);
+        }).base(), str.end());
+
+        int size = str.size();
+        
+        int length = 0;
+        
+        for (auto i = size - 1; i >= 0; i--)
+        {
+            if (str[i] != ' ')
+                length++;
+            else
+                break;
+        }
+
+        return length;
+    }
+};
+
+int main()
+{
+    Solution s;
+    std::cout << s.lengthOfLastWord("Hello World") << std::endl;
+    std::cout << s.lengthOfLastWord("   fly me   to   the moon  ") << std::endl;
+    std::cout << s.lengthOfLastWord("luffy is still joyboy") << std::endl;
+    std::cout << s.lengthOfLastWord("a") << std::endl;
+    std::cout << s.lengthOfLastWord(" ") << std::endl;
+    std::cout << s.lengthOfLastWord("day") << std::endl;
+    return 0;
+}
